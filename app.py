dca0bf8bce3942702de772f887870033e6259e1b
jaobalao <joao.victor@agoraentert.com.br>
2021-02-24 00:24:16 -0300

    Alterações nas imagens

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, template_folder='templates')
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test2.sqlite3'
 db = SQLAlchemy(app)
 
 # TABLE PRODUCTS
@@ -14,12 +14,14 @@ class Product(db.Model):
     description = db.Column(db.String)
     price = db.Column(db.Float)
     discount_percent = db.Column(db.Integer)
+    image = db.Column(db.String)
 
-    def __init__(self, name, description, price, discount_percent):
+    def __init__(self, name, description, price, discount_percent, image):
         self.name = name
         self.description = description
         self.price = price
         self.discount_percent = discount_percent
+        self.image = image
 
     def __repr__(self):
         return "<Product(name='%s', description='%s', price='%s', discount_percent='%s')>" % (self.name, self.description, self.price, self.discount_percent)
@@ -36,13 +38,14 @@ def add():
         description = request.form['description']
         price = request.form['price']
         discount_percent = request.form['discount_percent']
+        image = request.form['image']
         
         if price == '':
             price = 0
         if discount_percent == '':
             discount_percent = 0
                         
-        product = Product(name, description, price, discount_percent)
+        product = Product(name, description, price, discount_percent, image)
         db.session.add(product)
         db.session.commit()
         return redirect(url_for('index'))
@@ -56,6 +59,7 @@ def edit(id):
         product.description = request.form['description']
         product.price = request.form['price']
         product.discount_percent = request.form['discount_percent']
+        product.image = request.form['image']
 
         if product.price == '':
             product.price = 0
@@ -73,11 +77,6 @@ def delete(id):
     db.session.delete(product)
     db.session.commit()
     return redirect(url_for('index'))
-
-@app.route('/inspect/<int:id>', methods=['GET'])
-def get(id):
-    product = Product.query.get(id)
-    return render_template('inspect.html', product=product)
 
 
 if __name__ == '__main__':
